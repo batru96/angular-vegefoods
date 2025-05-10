813d9fbed2108ffe65562d0c200fbf3649d52973
Khoa Hoang Hoa <khoa.hoanghoa@nashtechglobal.com>
2025-05-10 13:38:53 +0700

    generate cart & checkout pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
   },
   {
     path: 'checkout',
