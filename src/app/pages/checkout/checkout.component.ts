10c51fc368cc5f6b4f8ff6914a7a0917a33d55ae
Khoa Hoang <khoa.hoanghoa@nashtechglobal.com>
2025-05-11 22:46:40 +0700

    Working on checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  checkoutForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    address: new FormGroup({
+      country: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      apartment: new FormControl(''), // optional
+      city: new FormControl('', Validators.required),
+      postcode: new FormControl('', Validators.required),
+    }),
+    phone: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+  })
 
+  countries = ['China', 'France', 'Germany', 'Italy', 'Japan', 'South Korea', 'Spain', 'United Kingdom', 'United States', 'Vietnam'];
+  
+
+  onSubmit() {
+    console.log(this.checkoutForm.value);
+    if (this.checkoutForm.valid) {
+      console.log('Form Submitted!', this.checkoutForm.value);
+      // Here you can handle the form submission, e.g., send the data to a server
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
